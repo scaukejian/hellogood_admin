2bdb5f999e7dcfa096f95751c77de93caa0b5411
scaukejian <954657344@qq.com>
2017-10-11 22:24:56 +0800

    Cookies字符串不允许出现逗号，用#号代替

--- src/main/java/com/hellogood/service/EmpRoleRelationService.java
+++ src/main/java/com/hellogood/service/EmpRoleRelationService.java
@@ -158,7 +158,7 @@ public class EmpRoleRelationService {
 		List<String> list = getRoleCodeListByEmpId(empId);
 		StringBuffer codes = new StringBuffer();
 		for (String code: list){
-			codes.append(code).append(",");
+			codes.append(code).append("#");
 		}
 		return codes.length() > 0 ? codes.substring(0, codes.length() - 1).toString() : codes.toString();
 	}
